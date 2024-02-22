5accb8a6cf6127d53570781f080217b28c02c9b1
mukhammadrizo <pulatovvmukhammadrizo@gmail.com>
2024-02-22 13:09:51 +0500

    js da10 ta misol

---
+++ app.js
@@ -0,0 +1,160 @@
+
+
+// function oddandEvent(num){
+//     if(num%2==0){
+//         console.log("juft")
+//     }
+//     else {
+//         console.log("toq son")
+//     }
+
+// }
+
+// oddandEvent(5)
+
+//MASALALAR
+
+//n1 Ikkita sonni bir-biriga qushish
+/*
+let x = +prompt("Birinchi sonni kiriting")
+let y = +prompt("Ikkinchi sonni kiriting")
+let natija = 0
+function sum(a,b){
+    natija = a+b
+    console.log(natija )
+}
+sum(x,y)
+*/
+
+//n2 Minutni sekundga aylantirish
+/* 
+let son = +prompt("Xohlagan minutingizni kiriting")
+let result = 0
+
+function sekund(a){
+    natija = a*60
+    console.log(son + " minut " +  natija + " sekundga teng")
+}
+sekund(son)
+ */
+
+//3 Kiritilgan songa 1 qushish
+/*
+let son = +prompt("Istalgan sonni kiriting")
+let natija = 0
+
+function son1(a){
+    natija = a + 1
+    console.log(natija)
+}
+
+son1(son)
+*/
+
+//n4 Uchburchakning yuzini topish
+/*
+let asos = +prompt("Uchburchakni asosini kiriting")
+let balandlik = +prompt("Uchburchakni balandligini kiriting") // h = balandlik
+let S = 0 // S = Yuzi
+
+function number(a,b){
+    S=(a*b)/2
+    console.log(S)
+}
+
+number(asos, balandlik)
+ */
+
+//n5 Yoshni kunlarda ifodalash
+
+/*
+let age = +prompt("Yoshingizni kiriting")
+let result = 0
+function kun(i){
+    result = i *365 
+    console.log(result)
+}
+
+kun(age)
+*/
+
+//n6 kubni topish
+/*
+let num = +prompt("Istalgan sonni kubini topish")
+let result 
+
+function kub (a){
+    result = a**3
+    console.log(result)
+}
+
+kub(num)
+ */
+
+//n7 Birinchi sonni qaytarish
+/*
+let num1 = +prompt("son 1")
+let num2 = +prompt("son 2")
+let num3 = +prompt("son 3")
+let num4 = +prompt("son 4")
+let result
+function massive(a,b,c,d){
+    result = a
+    console.log(result)
+}
+
+massive(num1,num2,num3,num4)
+*/
+  
+
+//  n8 Ikkita sonni kupaytmasi
+/*
+let num1 = +prompt("Birinchi sonni kiriting")
+let num2 = +prompt("Ikkinchi sonni kiriting")
+let result
+
+function power(a,b){
+    result = a * b
+    console.log(result)
+}
+
+power(num1,num2)
+*/
+
+
+
+//n9 soatni skeundga ugirib beradigan kod
+
+/*
+let soat = +prompt("Soatni kiriting")
+let result
+ 
+function sekund(a){
+    result = a * 60
+    result = result * 60
+    console.log(result + "sekund")
+}
+
+sekund(soat)
+*/
+
+
+
+//n10 uchburchakning eng uzun uchinchi  tomonini topish
+
+/*
+let num1 = +prompt("Uchburchakning birinchi tomonini kiriting")
+let num2 = +prompt("Uchburchakning ikkinchi tomonini kiriting")
+
+let result 
+
+function uchinchi_tomoni(a,b){
+    result = (a+b)-1
+    console.log("Uchinchi tomoni " + result)
+}
+
+uchinchi_tomoni(num1,num2)
+*/
+
+
+
